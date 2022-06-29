1027eb8575a1c530d3139ce6b0e3fba601c8628b
hviana <hv5088@gmail.com>
2022-06-29 17:32:12 -0300

    send project

---
+++ deps.ts
@@ -0,0 +1,45 @@
+export { Wordnet } from "https://raw.githubusercontent.com/hviana/wordnet_ts/main/mod.ts";
+export {
+  add,
+  and,
+  anyo,
+  appendo,
+  arrayo,
+  conso,
+  div,
+  emptyo,
+  eq,
+  facts,
+  fail,
+  firsto,
+  ge,
+  gt,
+  ImmutableMap,
+  isLVar,
+  le,
+  lt,
+  LVar,
+  lvar,
+  membero,
+  mul,
+  not,
+  numbero,
+  or,
+  resto,
+  run,
+  stringo,
+  sub,
+  succeed,
+  walk,
+} from "https://deno.land/x/logic_ts/mod.ts";
+
+export {
+  AMRUtils,
+} from "https://raw.githubusercontent.com/hviana/amr_utils/main/mod.ts";
+
+export type {
+  AMRGraph,
+} from "https://raw.githubusercontent.com/hviana/amr_utils/main/mod.ts";
+
+export { AMRQuery, QueryCore } from "https://deno.land/x/amr_query/mod.ts";
+export type { WordClass } from "https://raw.githubusercontent.com/hviana/wordnet_ts/main/mod.ts";
